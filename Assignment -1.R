2cb164d8c51a932540026175053b5c4936bfe296
AKC258 <chippaa@mail.sacredheart.edu>
2024-04-12 22:47:04 -0400

    first commit

---
+++ Assignment -1.R
@@ -0,0 +1,27 @@
+#Average of three numbers
+X <- c(7,12,5)
+Y <- mean(X)
+Y
+
+Greet<-"Welcome to R Progamming"
+Name<-"Arun Kumar"
+Message<-paste(Greet,Name)
+Message
+
+
+X=7
+Y=12
+Z=4
+
+if(X>Y){
+  print("X is Bigger than Y")
+} else {
+  print("X is lesser than Y")
+}
+
+if (X>Z){
+  print("X is Bigger than Z")
+} else {
+  print("X is lesser than Z")
+}
+
